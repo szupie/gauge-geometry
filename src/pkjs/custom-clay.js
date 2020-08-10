9575427f0dba63d121caf92fdf1f462cc95d80d5
Angus Lo <szupie@gmail.com>
2020-08-09 21:33:33 -0400

    added option to force weather update

--- src/pkjs/custom-clay.js
+++ src/pkjs/custom-clay.js
@@ -1,17 +1,16 @@
 module.exports = function(minified) {
-  var clayConfig = this;
-  var _ = minified._;
-  var $ = minified.$;
-  var HTML = minified.HTML;
+	var clayConfig = this;
+	var _ = minified._;
+	var $ = minified.$;
+	var HTML = minified.HTML;
 
-
-  clayConfig.on(clayConfig.EVENTS.AFTER_BUILD, function() {
-    clayConfig.getItemById('save-button').$element.set({
-      '$position': 'sticky',
-      '$bottom': '0',
-      '$backgroundColor': '#333333'
-    });
-    
-  });
-  
+	clayConfig.on(clayConfig.EVENTS.AFTER_BUILD, function() {
+		clayConfig.getItemById('save-button').$element.set({
+			'$position': 'sticky',
+			'$bottom': '0',
+			'$backgroundColor': '#333333'
+		});
+		
+	});
+	
 };
--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -76,7 +76,9 @@ Pebble.addEventListener('webviewclosed', function(e) {
 	const claySettings = clay.getSettings(e.response, false);
 	weatherAPIKey = claySettings['WEATHER_API_KEY'].value;
 	tempEnabled = claySettings['TEMP_ENABLED'].value;
-	// getWeather();
+	if (claySettings['UPDATE_WEATHER_ON_CONFIG'].value) {
+		getWeather();
+	}
 });
 
 // Listen for when the watchface is opened
