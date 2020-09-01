6d5ac99c9f5ef3b2ae4b214c7257d924bf227d4c
Angus Lo <szupie@gmail.com>
2020-09-01 18:36:50 -0400

    added diagram on configuration page to explain scales on temperature gauge

--- src/pkjs/custom-clay.js
+++ src/pkjs/custom-clay.js
@@ -21,6 +21,61 @@ module.exports = function(minified) {
 			'$backgroundRepeat': 'no-repeat'
 		});
 
+
+		// Style diagram for temperature dial
+		const tempUnitInput = clayConfig.getItemByMessageKey('TEMP_UNIT');
+		const dialDiameter = 7;
+		const markerThickness = 5;
+		$('#temp_dial').set({
+			$height: `${dialDiameter}rem`,
+			$width: `${dialDiameter}rem`,
+			$backgroundColor: '#666',
+			$borderRadius: '100%',
+			$margin: 'auto auto 1rem'
+		});
+
+		function updateTempDiagram() {
+			const tempUnit = tempUnitInput.get();
+			let tempAttr, angleMultiplier;
+			if (tempUnit === 'c') {
+				tempAttr = 'data-c';
+				angleMultiplier = 6;
+			} else {
+				tempAttr = 'data-f';
+				angleMultiplier = 3;
+			}
+			// extra $ marks necessary because they get dropped (escaped?)
+			$('#temp_explain .celsius').set('$$$$show', tempUnit === 'c');
+			$('#temp_explain .fahrenheit').set('$$$$show', tempUnit === 'f');
+
+			$('#temp_dial .label').each((item, index) => {
+				const degree = $(item).get(`@${tempAttr}`);
+				const angle = degree*angleMultiplier;
+				item.innerHTML = `<span class="text">${degree}°</span>`;
+				$(item).set({
+					$position: 'absolute',
+					$paddingTop: '0.1em',
+					$height: `${dialDiameter}rem`,
+					$width: '1px',
+					$left: `calc(50% + 2px - ${markerThickness}px / 2)`,
+					$transform: `rotate(${angle}deg)`,
+					$borderTop: `${markerThickness}px solid currentcolor`,
+					$fontSize: '0.75rem'
+				});
+				$('.text', item).set({
+					$display: 'inline-block',
+					$margin: 'auto',
+					$transform: `translateX(-50%) rotate(${-angle}deg)`
+				});
+			});
+		}
+		
+		tempUnitInput.on('change', function() {
+			updateTempDiagram();
+		});
+		updateTempDiagram();
+
+		// Dis/Enable weather options based on provider selection
 		const weatherEnabledToggle = clayConfig.getItemByMessageKey('TEMP_ENABLED');
 		const weatherProviderInput = clayConfig.getItemByMessageKey('WEATHER_PROVIDER');
 		const weatherElements = clayConfig.getItemsByGroup('weatherDetails');
