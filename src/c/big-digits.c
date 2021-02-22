60e8943d77f653c8b3b25dc3eb8e21cc0b85dd73
Angus Lo <szupie@gmail.com>
2021-02-21 21:31:37 -0500

    memory optimisation: revert back to loading digit bitmaps into memory only when needed

--- src/c/big-digits.c
+++ src/c/big-digits.c
@@ -17,33 +17,45 @@ static const int DIGIT_IMAGE_RESOURCE_IDS[] = {
 
 static GColor time_colour;
 
-static GBitmap *digit_bitmaps[10];
-static BitmapLayer *digit_layers[TOTAL_TIME_DIGITS];
-static unsigned short digit_values[TOTAL_TIME_DIGITS] = {UNDEFINED_DIGIT, UNDEFINED_DIGIT, UNDEFINED_DIGIT, UNDEFINED_DIGIT};
+static GBitmap *bitmaps[TOTAL_TIME_DIGITS];
+static BitmapLayer *layers[TOTAL_TIME_DIGITS];
+static unsigned short digit_values[TOTAL_TIME_DIGITS] = {
+	UNDEFINED_DIGIT, UNDEFINED_DIGIT, UNDEFINED_DIGIT, UNDEFINED_DIGIT
+};
 
 static PropertyAnimation *prop_anims[TOTAL_TIME_DIGITS];
 static Animation *anims[TOTAL_TIME_DIGITS];
 static bool init_animated = false;
 
 
-static void clear_slot(int slot_number) {
-	bitmap_layer_set_bitmap(
-		digit_layers[slot_number], 
-		gbitmap_create_blank(GSize(0, 0), GBitmapFormat1Bit)
-	);
-}
-
 // Loads the digit image from the application's resources and
 // displays it on-screen in the correct location.
 // Each slot is a quarter of the screen.
-static void load_digit_image_into_slot(int slot_number, int digit_value) {
-	GBitmap *bitmap_to_load = digit_bitmaps[digit_value];
+static void load_digit_image_into_slot(int slot, int digit_value) {
+	gbitmap_destroy(bitmaps[slot]);
 
-	bitmap_layer_set_bitmap(digit_layers[slot_number], bitmap_to_load);
+	if (digit_value != UNDEFINED_DIGIT) {
+		bitmaps[slot] = gbitmap_create_with_resource(
+			DIGIT_IMAGE_RESOURCE_IDS[digit_value]
+		);
+	} else {
+		bitmaps[slot] = gbitmap_create_blank(GSize(0, 0), GBitmapFormat1Bit);
+	}
+
+	bitmap_layer_set_bitmap(layers[slot], bitmaps[slot]);
+
+	#ifdef PBL_COLOR
+	replace_gbitmap_color(GColorBlack, time_colour, bitmaps[slot], NULL);
+	#elif PBL_BW
+	bitmap_layer_set_compositing_mode(layers[slot], GCompOpAnd);
+	if (gcolor_equal(time_colour, GColorWhite)) {
+		bitmap_layer_set_compositing_mode(layers[slot], GCompOpSet);
+	}
+	#endif
 
 	if (!init_animated) {
 		// initiate to bottom of frame
-		Layer *digit_layer = bitmap_layer_get_layer(digit_layers[slot_number]);
+		Layer *digit_layer = bitmap_layer_get_layer(layers[slot]);
 		GRect start_bounds = layer_get_bounds(digit_layer);
 		start_bounds.origin.y = start_bounds.size.h;
 		layer_set_bounds(digit_layer, start_bounds);
@@ -51,13 +63,15 @@ static void load_digit_image_into_slot(int slot_number, int digit_value) {
 		GPoint finish = GPoint(0, 0);
 
 		// set animation to frame top
-		prop_anims[slot_number] = property_animation_create_bounds_origin(digit_layer, NULL, &finish);
-		anims[slot_number] = property_animation_get_animation(prop_anims[slot_number]);
-		animation_set_curve(anims[slot_number], AnimationCurveEaseOut);
-		animation_set_delay(anims[slot_number], 100+40*slot_number);
-		animation_set_duration(anims[slot_number], 250);
+		prop_anims[slot] = property_animation_create_bounds_origin(
+			digit_layer, NULL, &finish
+		);
+		anims[slot] = property_animation_get_animation(prop_anims[slot]);
+		animation_set_curve(anims[slot], AnimationCurveEaseOut);
+		animation_set_delay(anims[slot], 100+40*slot);
+		animation_set_duration(anims[slot], 250);
 
-		animation_schedule(anims[slot_number]);
+		animation_schedule(anims[slot]);
 	}
 }
 
@@ -66,7 +80,7 @@ static void update_slot(int slot_number) {
 	if (digit_value != UNDEFINED_DIGIT) {
 		// unload digit if it is leading 0 of hour
 		if (digit_value == 0 && slot_number == 0) {
-			clear_slot(slot_number);
+			load_digit_image_into_slot(slot_number, UNDEFINED_DIGIT);
 		} else {
 			load_digit_image_into_slot(slot_number, digit_value);
 		}
@@ -88,26 +102,13 @@ static void set_row(unsigned short row_number, unsigned short value) {
 	}
 }
 
-static void load_digit_bitmaps() {
-	for (int i=0; i<10; i++) {
-		if (digit_bitmaps[i]) {
-			gbitmap_destroy(digit_bitmaps[i]);
-		}
-		digit_bitmaps[i] = gbitmap_create_with_resource(DIGIT_IMAGE_RESOURCE_IDS[i]);
-
-		#ifdef PBL_COLOR
-		replace_gbitmap_color(GColorBlack, time_colour, digit_bitmaps[i], NULL);
-		#endif
-	}
-}
-
 void init_digits(Layer *parent_layer) {
-	// load in digit bitmaps to memory
-	load_digit_bitmaps();
+	// load sample digit bitmap to determine size
+	bitmaps[0] = gbitmap_create_with_resource(DIGIT_IMAGE_RESOURCE_IDS[0]);
 
 	// determine positions of digits
 	GRect window_bounds = layer_get_bounds(parent_layer);
-	GRect tile_bounds = gbitmap_get_bounds(digit_bitmaps[0]);
+	GRect tile_bounds = gbitmap_get_bounds(bitmaps[0]);
 
 	const int tracking = -5;
 	const int x_offset = window_bounds.size.w/2 - (tile_bounds.size.w + tracking/2);
@@ -121,11 +122,11 @@ void init_digits(Layer *parent_layer) {
 			tile_bounds.size.h
 		);
 
-		digit_layers[slot_number] = bitmap_layer_create(frame);
-		bitmap_layer_set_compositing_mode(digit_layers[slot_number], GCompOpSet);
+		layers[slot_number] = bitmap_layer_create(frame);
+		bitmap_layer_set_compositing_mode(layers[slot_number], GCompOpSet);
 
 		// insert layers
-		Layer *digit_layer = bitmap_layer_get_layer(digit_layers[slot_number]);
+		Layer *digit_layer = bitmap_layer_get_layer(layers[slot_number]);
 		layer_add_child(parent_layer, digit_layer);
 	}
 }
@@ -148,27 +149,16 @@ void set_digits_colour(GColor colour) {
 	time_colour = colour;
 
 	// reload bitmaps to reset replaced colours
-	load_digit_bitmaps();
 	for (int slot_number=0; slot_number<TOTAL_TIME_DIGITS; slot_number++) {
 		update_slot(slot_number);
-
-		#ifdef PBL_BW
-		bitmap_layer_set_compositing_mode(digit_layers[slot_number], GCompOpAnd);
-		if (gcolor_equal(time_colour, GColorWhite)) {
-			bitmap_layer_set_compositing_mode(digit_layers[slot_number], GCompOpSet);
-		}
-		#endif
 	}
 }
 
 void destroy_digits() {
 	for (int i=0; i<TOTAL_TIME_DIGITS; i++) {
-		clear_slot(i);
-		layer_remove_from_parent(bitmap_layer_get_layer(digit_layers[i]));
-		bitmap_layer_destroy(digit_layers[i]);
-		digit_layers[i] = NULL;
-	}
-	for (int i=0; i<10; i++) {
-		gbitmap_destroy(digit_bitmaps[i]);
+		gbitmap_destroy(bitmaps[i]);
+		layer_remove_from_parent(bitmap_layer_get_layer(layers[i]));
+		bitmap_layer_destroy(layers[i]);
+		layers[i] = NULL;
 	}
 }
